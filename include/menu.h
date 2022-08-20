645a8908cf3a0679648697161c1de99e1b128efe
john <1142008606slam@gmail.com>
2022-08-20 11:26:46 +0800

    增加版本信息，学习cmake

--- include/menu.h
+++ include/menu.h
@@ -1,4 +1,14 @@
+/*
+ * @Author: john 1142008606slam@gmail.com
+ * @Date: 2022-08-17 22:19:14
+ * @LastEditors: john 1142008606slam@gmail.com
+ * @LastEditTime: 2022-08-20 11:10:00
+ * @FilePath: /LinjokGaming/include/menu.h
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #pragma once
+#include <iostream>
+#include "configure.h"
 
 class Menu
 {
@@ -17,4 +27,6 @@ public:
         static Menu instance;
         return instance;      
     }
+    public:
+    void showVersion();
 };
--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+/*
+ * @Author: john 1142008606slam@gmail.com
+ * @Date: 2022-08-17 22:19:14
+ * @LastEditors: john 1142008606slam@gmail.com
+ * @LastEditTime: 2022-08-20 11:10:30
+ * @FilePath: /LinjokGaming/main.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #include <iostream>
 #include "menu.h"
 using namespace std;
@@ -8,5 +16,6 @@ int main(int argc, char **argv)
     cout << "-+-+-linjok Gaming!-+-+-" << endl;
     cout << "rock-paper-scissors" << endl;
     Menu &gameMenu = Menu::getInstance();
+    gameMenu.showVersion();
     return 0;
 }
