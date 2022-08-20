e63321611ca9c2de545e3d1a12f6a3259424bd8f
john <1142008606slam@gmail.com>
2022-08-20 22:18:27 +0800

    add funcs

--- games/games.h
+++ games/games.h
@@ -2,8 +2,26 @@
  * @Author: john 1142008606slam@gmail.com
  * @Date: 2022-08-20 17:26:10
  * @LastEditors: john 1142008606slam@gmail.com
- * @LastEditTime: 2022-08-20 17:26:45
+ * @LastEditTime: 2022-08-20 22:14:48
  * @FilePath: /LinjokGaming/games/games.h
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 #pragma once
+
+class Games
+{
+private:
+    /* data */
+public:
+    Games(/* args */);
+    ~Games();
+};
+
+Games::Games(/* args */)
+{
+}
+
+Games::~Games()
+{
+}
+
--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
  * @Author: john 1142008606slam@gmail.com
  * @Date: 2022-08-17 22:19:14
  * @LastEditors: john 1142008606slam@gmail.com
- * @LastEditTime: 2022-08-20 17:00:49
+ * @LastEditTime: 2022-08-20 22:10:41
  * @FilePath: /LinjokGaming/main.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 #include <iostream>
 #include "menu.h"
+#include "games.h"
 using namespace std;
 
 int main(int argc, char **argv)
